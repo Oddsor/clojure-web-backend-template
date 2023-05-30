81e465edc221640aae5c9fc6da7b5f1e0a47fae3
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2023-05-30 07:43:10 +0200

    Reorder middleware

--- src/simple_web/base_router.clj
+++ src/simple_web/base_router.clj
@@ -21,14 +21,21 @@
                                    form-format)))
             :coercion rm/coercion
             ; Order of middleware is important.
-            :middleware [; This ensures query params are handled
+            :middleware [; This adds exception handling.
+                         ; Default handler returns a clojure-map which won't be correctly formatted,
+                         ; so we override that one. (muuntaja would handle maps, but can't be placed above the
+                         ; exception middleware because otherwise formatting errors won't be caught!)
+                         (re/create-exception-middleware
+                          (assoc re/default-handlers
+                                 ::re/default (fn [^Exception e _]
+                                                {:status 500
+                                                 :body (str "Unknown error occurred: " (ex-message e))})))
+                         ; This ensures query params are handled 
                          rp/parameters-middleware
                          ; Format data on the way in and out
                          mm/format-middleware
-                         ; This adds exception handling
-                         re/exception-middleware
                          ; TODO maybe tracing should not log exceptions here? If so, place at top
-                         ; Logging (place below exception handling, or trace will assume the call was a success)
+                         ; Logging (place BELOW exception handling, or trace will assume the call was a success)
                          logging/trace-middleware
                          ; Data validation middleware
                          rc/coerce-exceptions-middleware
